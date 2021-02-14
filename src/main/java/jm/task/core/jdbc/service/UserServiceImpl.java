816ce896ad6c8b9a5b27bca3a184635a19957bd2
Евгений Кашицын <gekan73@mail.ru>
2021-02-14 10:45:18 +0400

    version prepare 14.02.2021

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,83 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        String create = "CREATE TABLE users (id LONG PRIMARY KEY AUTO_INCREMENT NOT NULL , name VARCHAR (20) NOT NULL , lastName VARCHAR (20) NOT NULL , age BYTE)";
+        Util util = new Util();
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(create);
+        } catch (SQLException throwables) {
+            System.out.println("Exception create");
+        }
     }
 
     public void dropUsersTable() {
-
+        String drop = "DROP TABLE [IF EXISTS ] users";
+        Util util = new Util();
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(drop);
+        } catch (SQLException throwables) {
+            System.out.println("Exception drop");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String save = "INSERT users(name, lastName, age) VALUES ("+name+", "+lastName+", "+age+")";
+        Util util = new Util();
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(save);
+        } catch (SQLException throwables) {
+            System.out.println("Exception insert");
+        }
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        String remove = "DELETE FROM users WHERE id=" + id;
+        Util util = new Util();
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(remove);
+        } catch (SQLException throwables) {
+            System.out.println("Exception remove");
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        Util util = new Util();
+        List<User> list = new ArrayList<>();
+        try {
+            ResultSet resultSet = util.getConnection().createStatement().executeQuery("SELECT *FROM users");
+            while (resultSet.next()) {
+                list.add(new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age")));
+            }
+        } catch (SQLException throwables) {
+            System.out.println("Exception getAllUsers");
+        }
+        return list;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        String clean = "TRUNCATE TABLE users";
+        Util util = new Util();
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(clean);
+        } catch (SQLException throwables) {
+            System.out.println("Exception clean");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "3157860";
+
+    Connection connection = null;
+    public Util() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение установлено");
+            }
+        } catch (SQLException | ClassNotFoundException | NoSuchMethodException ex) {
+            System.err.println("Не удалось загрузить драйвер!");
+            ex.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }

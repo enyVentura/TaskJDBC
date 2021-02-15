6cc815b631a8bd4075472b0ecf165e6c2c853d95
Евгений Кашицын <gekan73@mail.ru>
2021-02-15 22:04:12 +0400

    version prepare 15.02.2021

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,19 +3,17 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
         String create = "CREATE TABLE users (id LONG PRIMARY KEY AUTO_INCREMENT NOT NULL , name VARCHAR (20) NOT NULL , lastName VARCHAR (20) NOT NULL , age BYTE)";
-        Util util = new Util();
         try {
-            Statement statement = util.getConnection().createStatement();
-            statement.executeUpdate(create);
+            PreparedStatement preparedStatement=new Util().getConnection().prepareStatement(create);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
         } catch (SQLException throwables) {
             System.out.println("Exception create");
         }
@@ -23,21 +21,21 @@ public class UserServiceImpl implements UserService {
 
     public void dropUsersTable() {
         String drop = "DROP TABLE [IF EXISTS ] users";
-        Util util = new Util();
         try {
-            Statement statement = util.getConnection().createStatement();
-            statement.executeUpdate(drop);
+            PreparedStatement preparedStatement=new Util().getConnection().prepareStatement(drop);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
         } catch (SQLException throwables) {
             System.out.println("Exception drop");
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String save = "INSERT users(name, lastName, age) VALUES ("+name+", "+lastName+", "+age+")";
-        Util util = new Util();
+        String save = "INSERT INTO users (name, lastName, age) VALUES ("+name+", "+lastName+", "+age+")";
         try {
-            Statement statement = util.getConnection().createStatement();
-            statement.executeUpdate(save);
+            PreparedStatement preparedStatement=new Util().getConnection().prepareStatement(save);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
         } catch (SQLException throwables) {
             System.out.println("Exception insert");
         }
@@ -46,20 +44,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public void removeUserById(long id) {
         String remove = "DELETE FROM users WHERE id=" + id;
-        Util util = new Util();
         try {
-            Statement statement = util.getConnection().createStatement();
-            statement.executeUpdate(remove);
+            PreparedStatement preparedStatement=new Util().getConnection().prepareStatement(remove);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
         } catch (SQLException throwables) {
             System.out.println("Exception remove");
         }
     }
 
     public List<User> getAllUsers() {
-        Util util = new Util();
         List<User> list = new ArrayList<>();
         try {
-            ResultSet resultSet = util.getConnection().createStatement().executeQuery("SELECT *FROM users");
+            ResultSet resultSet = new Util().getConnection().createStatement().executeQuery("SELECT * FROM users");
             while (resultSet.next()) {
                 list.add(new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age")));
             }
@@ -72,10 +69,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void cleanUsersTable() {
         String clean = "TRUNCATE TABLE users";
-        Util util = new Util();
         try {
-            Statement statement = util.getConnection().createStatement();
-            statement.executeUpdate(clean);
+            PreparedStatement preparedStatement=new Util().getConnection().prepareStatement(clean);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
         } catch (SQLException throwables) {
             System.out.println("Exception clean");
         }
